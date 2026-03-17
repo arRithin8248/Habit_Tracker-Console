b6564d7eebac62e0cdadb9a0e4bd9dc521c80597
arRithin8248 <rithinraghu@gmail.com>
2026-03-17 09:31:59 +0530

    Error debug

--- HabitTracker.py
+++ HabitTracker.py
@@ -1,14 +1,22 @@
 from user import register_user, view_users
 from habit import create_habit, view_habits
+from progress import track_progress
+from remainder import add_reminder
+from reward import give_reward
+from journal import add_journal
 
 while True:
 
-    print("\n==== HABIT TRACKER ====")
+    print("\n===== HABIT TRACKER =====")
     print("1 Register User")
     print("2 Create Habit")
-    print("3 View Users")
-    print("4 View Habits")
-    print("5 Exit")
+    print("3 Track Progress")
+    print("4 Add Reminder")
+    print("5 Give Reward")
+    print("6 Add Journal")
+    print("7 View Users")
+    print("8 View Habits")
+    print("9 Exit")
 
     choice = int(input("Enter choice: "))
 
@@ -19,10 +27,22 @@ while True:
         create_habit()
 
     elif choice == 3:
-        view_users()
+        track_progress()
 
     elif choice == 4:
-        view_habits()
-        
+        add_reminder()
+
     elif choice == 5:
+        give_reward()
+
+    elif choice == 6:
+        add_journal()
+
+    elif choice == 7:
+        view_users()
+
+    elif choice == 8:
+        view_habits()
+
+    elif choice == 9:
         break
